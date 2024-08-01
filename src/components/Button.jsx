c576fa2cb0db635b0dd86f5ff919a664822e0d43
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2024-08-01 14:47:11 +0700

    member list

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button(){
+    return(
+        <div className=" px-4 ">
+          <button className="bg-blue-500 shadow-lg shadow-indigo-500/50 text-white py-2 px-6 rounded">View</button>
+        </div>
+
+    )
+}
+export default Button;
--- src/pages/member/MemberPage.jsx
+++ src/pages/member/MemberPage.jsx
@@ -1,9 +1,68 @@
+import React,{useState, useEffect} from "react";
+import Button from "../../components/Button";
+
 const MemberPage = () => {
+  const [members, setMembers] = useState([]);
+  const apiUrl = 'http://localhost:3000/api/members';
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImlhdCI6MTcyMjQ5MzU1OCwiZXhwIjoxNzIyNTI5NTU4fQ.AkEqOFJqzdtzy_kXexHeaF3xaafsECO6IjHEtNrwCI4';
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+          }
+        });
+        const data = await response.json();
+        setMembers(data);
+        console.log(data);
+        return response.data
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMembers(); 
+  },[])
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Member</h1>
+    <div className="bg-gray-100">
+      <h1 className="text-3xl font-bold ms-14 text-2xl">Member</h1>
+      <td className="py-2 px-4">
+        <button className="bg-blue-500 shadow-lg shadow-indigo-500/50 text-white py-2 px-8 rounded hover:bg-gray-600 ms-10">
+        Create
+        </button>
+      </td>
+      <div className="overflow-hidden rounded-xl border border-gray-500">
+    <table class="table-fixed border-collapse border border-slate-800 border-spacing-2 shadow-2xl w-full text-center divide-y-4 divide-slate-400/25 mt-4">
+  <thead>
+    <tr>
+      <th className="p-5 border border-slate-300 bg-slate-200">Action</th>
+      <th className="p-5 border border-slate-300 bg-slate-200">Member Code</th>
+      <th className="p-5 border border-slate-300 bg-slate-200">Fullname</th>
+      <th className="p-5 border border-slate-300 bg-slate-200">Phone</th>
+      <th className="p-5 border border-slate-300 bg-slate-200">Address</th>
+      <th className="p-5 border border-slate-300 bg-slate-200">Start Date</th>
+      <th className="p-5 border border-slate-300 bg-slate-200">Expiry Date</th>
+    </tr>
+  </thead>
+  <tbody>
+    {members.map((item) =>(
+    <tr key={item.id}>
+      <td className="p-3 border border-slate-300">{<Button/>}</td>
+      <td className="p-3 border border-slate-300">{item.member_code}</td>
+      <td className="p-3 border border-slate-300">{item.fullname}</td>
+      <td className="p-3 border border-slate-300">{item.phone_number}</td>
+      <td className="p-3 border border-slate-300">{item.address}</td>
+      <td className="p-3 border border-slate-300">{item.start_date}</td>
+      <td className="p-3 border border-slate-300">{item.expiry_date}</td>
+    </tr>
+    ))}
+  </tbody>
+  </table>
+  </div>
     </div>
   );
-};
+}
 
 export default MemberPage;
