a6b5684f069c3284531f52b6ab911c81870f3c6c
KhoeunSopheak <sopheak.khoeun@instile.pse.ngo>
2024-08-13 08:52:15 +0700

    add style

--- src/pages/member/MemberPage.jsx
+++ src/pages/member/MemberPage.jsx
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from "react";
 import Button from "../../components/Button";
+import { Link } from "react-router-dom";
 
 const MemberPage = () => {
   const [members, setMembers] = useState([]);
@@ -27,35 +28,35 @@ const MemberPage = () => {
   },[])
   return (
     <div className="bg-gray-100">
-      <h1 className="text-3xl font-bold ms-14 text-2xl">Member</h1>
+      <h1 className="text-3xl font-bold ms-7">Member</h1>
       <td className="py-2 px-4">
-        <button className="bg-blue-500 shadow-lg shadow-indigo-500/50 text-white py-2 px-8 rounded hover:bg-gray-600 ms-10">
-        Create
+        <button className="bg-blue-500 shadow-lg shadow-indigo-500/50 text-white py-2 px-8 rounded hover:bg-gray-600 ms-4">
+        <Link to ='/member/new'>Create</Link>
         </button>
       </td>
       <div className="overflow-hidden rounded-xl border border-gray-500">
-    <table class="table-fixed border-collapse border border-slate-800 border-spacing-2 shadow-2xl w-full text-center divide-y-4 divide-slate-400/25 mt-4">
-  <thead>
+    <table class="min-w-full">
+  <thead className="bg-gray-300">
     <tr>
-      <th className="p-5 border border-slate-300 bg-slate-200">Action</th>
-      <th className="p-5 border border-slate-300 bg-slate-200">Member Code</th>
-      <th className="p-5 border border-slate-300 bg-slate-200">Fullname</th>
-      <th className="p-5 border border-slate-300 bg-slate-200">Phone</th>
-      <th className="p-5 border border-slate-300 bg-slate-200">Address</th>
-      <th className="p-5 border border-slate-300 bg-slate-200">Start Date</th>
-      <th className="p-5 border border-slate-300 bg-slate-200">Expiry Date</th>
+      <th className="py-4 px-4 text-center">Action</th>
+      <th className="py-4 px-6 text-center">Member Code</th>
+      <th className="py-4 px-6 text-center">Fullname</th>
+      <th className="py-4 px-6 text-center">Phone</th>
+      <th className="py-4 px-6 text-center">Address</th>
+      <th className="py-4 px-6 text-center">Start Date</th>
+      <th className="py-4 px-6 text-center">Expiry Date</th>
     </tr>
   </thead>
   <tbody>
     {members.map((item) =>(
-    <tr key={item.id}>
-      <td className="p-3 border border-slate-300">{<Button/>}</td>
-      <td className="p-3 border border-slate-300">{item.member_code}</td>
-      <td className="p-3 border border-slate-300">{item.fullname}</td>
-      <td className="p-3 border border-slate-300">{item.phone_number}</td>
-      <td className="p-3 border border-slate-300">{item.address}</td>
-      <td className="p-3 border border-slate-300">{item.start_date}</td>
-      <td className="p-3 border border-slate-300">{item.expiry_date}</td>
+    <tr key={item.id} className="border-b border-slate-700">
+      <td className="py-4 px-8">{<Button/>}</td>
+      <td className="py-4 px-8">{item.member_code}</td>
+      <td className="py-4 px-8">{item.fullname}</td>
+      <td className="py-4 px-8">{item.phone_number}</td>
+      <td className="py-4 px-8">{item.address}</td>
+      <td className="py-4 px-8">{item.start_date}</td>
+      <td className="py-4 px-8">{item.expiry_date}</td>
     </tr>
     ))}
   </tbody>
